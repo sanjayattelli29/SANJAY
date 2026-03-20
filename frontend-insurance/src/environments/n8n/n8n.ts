b2311b2a1eead1bdc264acf8f8afb09951e2b2f3
Sanjay <attellisanjay29@gmail.com>
2026-03-20 12:29:50 +0530

    vertex deployement done and shifted to n8n

--- frontend-insurance/src/environments/n8n/n8n.ts
+++ frontend-insurance/src/environments/n8n/n8n.ts
@@ -1,7 +1,7 @@
 export const n8nWebhooks = {
     chatbotBot: 'https://sanjay29n8n.app.n8n.cloud/webhook/chatbot-bot-1',
     chatbotAgent: 'https://sanjay29n8n.app.n8n.cloud/webhook/chatbot-agent-1',
-    sendOtp: 'https://sanjay29n8n.app.n8n.cloud/webhook-test/send-otp',
+    sendOtp: 'https://sanjay29n8n.app.n8n.cloud/webhook/send-otp',
     claimAiInsights: 'https://sanjay29n8n.app.n8n.cloud/webhook/claim-ai-insights',
     agentSendEmail: 'https://sanjay29n8n.app.n8n.cloud/webhook/agent-send-email',
     agentAiInsights: 'https://sanjay29n8n.app.n8n.cloud/webhook/agent-ai-insights',
